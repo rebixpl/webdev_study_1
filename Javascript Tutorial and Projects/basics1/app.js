9dc866b97003e551271a71d63c71a02c7da0aa57
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-06-19 22:24:33 +0200

    js basics 2

---
+++ Javascript Tutorial and Projects/basics1/app.js
@@ -0,0 +1,37 @@
+// document.write("document write hello");
+// alert("hello people");
+// console.log("hello world console log");
+
+// variables
+// let name = "xd xdd xddd 2";
+// console.log(name);
+
+let address, zip, state;
+
+address = "Jana Pawła 2";
+
+console.log(address);
+
+// VAR VS CONST VS LET
+// old way of declaring variables (since 1995)
+var value = "some value";
+
+// it was introduced in i believe 2015 or sth thus
+// let and const should be used instead of var
+let name = "John";
+
+// people generally use const as much as possible, but
+// when they know that data will change they prefer using
+// let from var
+const lastName = "Johnson";
+
+const website = "youtube";
+const url = "https://www." + website + ".com";
+console.log(url);
+
+const num = 34;
+const num2 = 2.324;
+
+const addSum = num + num2;
+
+console.log(addSum);

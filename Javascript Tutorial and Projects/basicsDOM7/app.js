0958b6fa2e9d8c8d0c86082c982c3b2e1e2abec4
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-06-21 23:11:01 +0200

    js basics DOM 21

--- Javascript Tutorial and Projects/basicsDOM7/app.js
+++ Javascript Tutorial and Projects/basicsDOM7/app.js
@@ -12,6 +12,44 @@ random.classList.add("title");
 const heading = document.querySelector("h2");
 const btn = document.querySelector(".btn");
 
+function changeColors() {
+  //   heading.classList.toggle("red");
+  let hasClass = heading.classList.contains("red");
+  if (hasClass) {
+    heading.classList.remove("red");
+  } else {
+    heading.classList.add("red");
+  }
+}
+
+btn.addEventListener("click", changeColors);
+
+// Mouse Events
+// click - fires after full action occurs, happens when both of mousedown and mouseup happens
+// mousedown - button is pressed
+// mouseup - button is released
+// mouseenter - moved onto an element
+// mouseleave - moved out of an element
+
+const heading2 = document.querySelector("h1");
+const btn2 = document.querySelector(".btn");
+
 btn.addEventListener("click", function () {
-  heading.classList.toggle("red");
+  console.log("click");
+});
+
+btn.addEventListener("mousedown", function () {
+  console.log("mousedown");
+});
+
+btn.addEventListener("mouseup", function () {
+  console.log("mouseup");
+});
+
+heading.addEventListener("mouseenter", function () {
+  heading.classList.add("blue");
+});
+
+heading.addEventListener("mouseleave", function () {
+  heading.classList.remove("blue");
 });

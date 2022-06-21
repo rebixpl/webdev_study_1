7d9e2611474ef27ba3cc862bb4730bbcda70c39b
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-06-21 20:12:53 +0200

    js basics DOM 20

--- Javascript Tutorial and Projects/basicsDOM7/app.js
+++ Javascript Tutorial and Projects/basicsDOM7/app.js
@@ -3,3 +3,15 @@ const random = document.querySelector(".random");
 
 random.style.textTransform = "capitalize";
 random.classList.add("title");
+
+// Click Event
+// select element
+// addEventListener()
+// what event, what to do
+
+const heading = document.querySelector("h2");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function () {
+  heading.classList.toggle("red");
+});

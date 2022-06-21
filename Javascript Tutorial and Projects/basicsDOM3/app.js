fe83140d3a5b4a8d4f02996647ddb269164dea54
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-06-21 12:05:38 +0200

    js basics DOM 15

--- Javascript Tutorial and Projects/basicsDOM3/app.js
+++ Javascript Tutorial and Projects/basicsDOM3/app.js
@@ -16,3 +16,45 @@ h1.style.color = "red";
 const btn = document.getElementById("btn");
 btn.style.backgroundColor = "blue";
 btn.style.color = "white";
+
+// getElementsByTagName('tagname');
+// node-list = array-like object
+// index, length property, but not array methods
+
+const headings = document.getElementsByTagName("h2");
+headings[0].style.color = "yellow";
+console.log(headings.length); // 2
+
+const items = document.getElementsByTagName("li");
+// transform node-list to array
+items[2].style.color = "orange";
+const betterItems = [...items];
+betterItems[3].style.color = "violet";
+// now we can use forEach on our new betterItems array
+betterItems.forEach(function (item) {
+  console.log(item);
+});
+
+// getElementsByClassName('classname');
+// node-list = array-like object
+// index, length property, but not array methods
+const listItems = document.getElementsByClassName("special");
+listItems[0].style.color = "aquamarine";
+
+// querySelector('any css'); - selects single first element
+// querySelectorAll('any css'); - selects all
+// with querySelectorAll we can use forEach method
+// with queryselectors we need to pass # for and ID and . for a class
+const result3 = document.querySelector("#result");
+result3.style.backgroundColor = "wheat";
+
+const lastItem = document.querySelector("li:last-child");
+console.log(lastItem);
+lastItem.style.backgroundColor = "white";
+
+const list = document.querySelectorAll(".special");
+
+list.forEach(function (item) {
+  console.log(item);
+  item.style.color = "yellow";
+});

6c3729c17b91f5aab37255dd9ed441a0681c5a78
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-06-23 09:03:52 +0200

    js projects DOM 27 | color flipper - simple js and html

--- Javascript Tutorial and Projects/javascript-basic-projects-master/01-color-flipper/setup/app.js
+++ Javascript Tutorial and Projects/javascript-basic-projects-master/01-color-flipper/setup/app.js
@@ -1,1 +1,31 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = [
+  "green",
+  "red",
+  "rgba(133,122,200)",
+  "#f15025",
+  "purple",
+  "aquamarina",
+  "yellowgreen",
+];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  let randomNum = getRandomNumber();
+  // check if the color is the same as before, if it is then generate a
+  // new random index until it is different from before
+  while (randomNum === colors.indexOf(color.textContent)) {
+    randomNum = getRandomNumber();
+  }
+  const randomColor = colors[randomNum];
+  document.body.style.backgroundColor = randomColor;
+  color.textContent = randomColor;
+  console.log(randomColor);
+  console.log(randomNum);
+});
+
+function getRandomNumber() {
+  // generate random number from 0 to colors.length -1 (because of indexes)
+  return Math.ceil(Math.random() * colors.length) - 1;
+}

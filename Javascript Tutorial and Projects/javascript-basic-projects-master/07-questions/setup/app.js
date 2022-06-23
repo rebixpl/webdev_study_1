316f9bb9809bddfe718c9083ff0646687f53d031
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-06-23 21:20:50 +0200

    js projects DOM 34 | questions HTML / JS / traversing the DOM method

--- Javascript Tutorial and Projects/javascript-basic-projects-master/07-questions/setup/app.js
+++ Javascript Tutorial and Projects/javascript-basic-projects-master/07-questions/setup/app.js
@@ -1,2 +1,18 @@
 //using selectors inside the element
 // traversing the dom
+
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", buttonClicked);
+});
+
+/**
+ *
+ * @param {Event} e
+ */
+function buttonClicked(e) {
+  /** @type {Element}*/
+  const questionEl = e.currentTarget.parentElement.parentElement;
+  questionEl.classList.toggle("show-text");
+}

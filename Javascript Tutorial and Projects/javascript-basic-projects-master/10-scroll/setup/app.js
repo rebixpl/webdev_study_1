e61ccd11714fb7bdd38a0c0d246cf3acc0be5883
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-06-25 23:13:25 +0200

    js projects DOM 43 | scroll project / JS toggle dynamic setup + fixed navbar when scrolling the page

--- Javascript Tutorial and Projects/javascript-basic-projects-master/10-scroll/setup/app.js
+++ Javascript Tutorial and Projects/javascript-basic-projects-master/10-scroll/setup/app.js
@@ -13,10 +13,39 @@ const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", function () {
-  linksContainer.classList.toggle("show-links");
+  //   linksContainer.classList.toggle("show-links"); // simple navtoggle setup
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  // console.log(containerHeight); // 0 // 0 because we set height of parent div with class
+  // '.links-container' inside css to 0
+  const linksHeight = links.getBoundingClientRect().height;
+  // console.log(linksHeight); // 201.5625
+
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = `0px`;
+  }
 });
 
 // ********** fixed navbar ************
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.scrollY;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+
+  if (scrollHeight > 500) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
 
 // ********** smooth scroll ************
 // select links

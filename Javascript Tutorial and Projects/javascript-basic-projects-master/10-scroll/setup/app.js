371eb553388dd2dc0e1f696b89a7d4b0b376029f
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-06-25 23:36:15 +0200

    js projects DOM 44 | scroll project /JS smooth scroll

--- Javascript Tutorial and Projects/javascript-basic-projects-master/10-scroll/setup/app.js
+++ Javascript Tutorial and Projects/javascript-basic-projects-master/10-scroll/setup/app.js
@@ -49,3 +49,38 @@ window.addEventListener("scroll", function () {
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    // prevent default ( links will no longer scroll to the section)
+    e.preventDefault();
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1); // for example href="#home"
+    // slice cuts the '#' without modifying the original string
+    const element = document.getElementById(id);
+    // calculate the heights
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    // scroll the page
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+
+    // after scrolling to the desired position, simply close the menu
+    // ( because on mobile it takes halve of a screen real estate)
+    linksContainer.style.height = 0;
+  });
+});

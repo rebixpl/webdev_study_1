c3df8bfdb51823298485034cd53cf894ce81c9af
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-06-28 17:15:54 +0200

    js projects DOM 52 | grocery bud / js

--- Javascript Tutorial and Projects/javascript-basic-projects-master/14-grocery-bud/setup/app.js
+++ Javascript Tutorial and Projects/javascript-basic-projects-master/14-grocery-bud/setup/app.js
@@ -1,11 +1,43 @@
 // ****** SELECT ITEMS **********
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery"); // input
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
 
 // edit option
+let editElement;
+let editFlag = false;
+let editID = "";
 
 // ****** EVENT LISTENERS **********
+// submit form
+form.addEventListener("submit", addItem);
 
 // ****** FUNCTIONS **********
+/**
+ *
+ * @param {Event} e
+ */
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value; //console.log(value); // for example: eggs
+  // in more serious projects we should use external libraries to
+  // generate ids XD
+  const id = new Date().getTime().toString(); // console.log(id); // 1656427129206
 
+  // !value === (value !== "")
+  // !editFlag === false
+  if (value && !editFlag) {
+    console.log("add item to the list");
+  } else if (value && editFlag) {
+    console.log("editing");
+  } else {
+    console.log("empty value");
+  }
+}
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********

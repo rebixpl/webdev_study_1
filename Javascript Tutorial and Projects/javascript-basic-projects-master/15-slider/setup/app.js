74f2b0c6733dcd4ce21951fe07351648fa9c18f8
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-06-30 08:44:07 +0200

    js projects DOM 60 | simple slider
    1. slides approach
    2.

--- Javascript Tutorial and Projects/javascript-basic-projects-master/15-slider/setup/app.js
+++ Javascript Tutorial and Projects/javascript-basic-projects-master/15-slider/setup/app.js
@@ -6,6 +6,33 @@ slides.forEach(function (slide, index) {
   slide.style.left = `${index * 100}%`;
 });
 
+let counter = 0;
+
+nextBtn.addEventListener("click", function () {
+  counter++;
+  carousel();
+});
+
+prevBtn.addEventListener("click", function () {
+  counter--;
+  carousel();
+});
+
+function carousel() {
+  //   console.log(counter);
+  //   console.log(slides.length);
+  if (counter === slides.length) {
+    counter = 0;
+  }
+  if (counter < 0) {
+    counter = slides.length - 1;
+  }
+
+  slides.forEach(function (slide) {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+}
+
 /* .slide:nth-child(1){
   left: 0;
 }

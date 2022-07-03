140f5cb69743efe42feacb9ec54e7586035bd590
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-07-03 14:07:30 +0200

    js OOP PROJECTS 71 | counter
    1. Counter - select elements
    2. Counter - Functions

--- Javascript Tutorial and Projects/javascript-basic-projects-master/16-counter/starter/app.js
+++ Javascript Tutorial and Projects/javascript-basic-projects-master/16-counter/starter/app.js
@@ -1,10 +1,3 @@
-function Counter(element, value) {
-  console.log(element, value);
-}
-
-const firstCounter = new Counter(getElement(".first-counter"), 100);
-const secondCounter = new Counter(getElement(".second-counter"), 400);
-
 function getElement(selection) {
   const element = document.querySelector(selection);
   if (element) {
@@ -15,3 +8,41 @@ function getElement(selection) {
     );
   }
 }
+
+function Counter(element, value) {
+  this.counter = element;
+  this.value = value;
+  this.startingValue = value;
+  this.resetBtn = element.querySelector(".reset");
+  this.increaseBtn = element.querySelector(".increase");
+  this.decreaseBtn = element.querySelector(".decrease");
+  this.valueDOM = element.querySelector(".value");
+  this.valueDOM.textContent = this.value;
+}
+
+Counter.prototype.increase = function () {
+  this.value++;
+  this.valueDOM.textContent = this.value;
+  console.log(this);
+};
+
+Counter.prototype.reset = function () {
+  this.value = this.startingValue;
+  this.valueDOM.textContent = this.value;
+};
+
+Counter.prototype.decrease = function () {
+  this.value--;
+  this.valueDOM.textContent = this.value;
+};
+
+const firstCounter = new Counter(getElement(".first-counter"), 100);
+const secondCounter = new Counter(getElement(".second-counter"), 400);
+
+firstCounter.increase();
+firstCounter.decrease();
+firstCounter.decrease();
+firstCounter.reset();
+firstCounter.decrease();
+firstCounter.decrease();
+firstCounter.reset();

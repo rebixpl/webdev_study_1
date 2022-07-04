8ab6e62757fbb56a61c2b253c463c7a596f7037d
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-07-04 21:42:57 +0200

    js OOP PROJECTS 73 | gallery
    1. intro
    2. HTML
    3. gallery - select elements

--- Javascript Tutorial and Projects/javascript-basic-projects-master/17-gallery/starter/app.js
+++ Javascript Tutorial and Projects/javascript-basic-projects-master/17-gallery/starter/app.js
@@ -7,3 +7,19 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
+
+// constructor function
+function Gallery(element) {
+  // using spread brackets [ES6] to create array from nodeList
+  this.list = [...element.querySelectorAll(".img")];
+  // target
+  this.modal = getElement(".modal");
+  this.modalImg = getElement(".main-img");
+  this.modalImages = getElement(".modal-images");
+  this.closeBtn = getElement(".close-btn");
+  this.nextBtn = getElement(".next-btn");
+  this.prevBtn = getElement(".prev-btn");
+}
+
+const nature = new Gallery(getElement(".nature"));
+const city = new Gallery(getElement(".city"));

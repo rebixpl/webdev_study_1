4264709fd8baf2cc5288c68d51845827d990d06c
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-07-06 14:13:06 +0200

    js study ES6 88 | ES6
    1. Spread Operator - Basics
    2.  Spread Operator - Objects
    3. Spread Operator - DOM Elements
    4. Spread Operator - Functions Arguments

--- Javascript Tutorial and Projects/basicsES6_5/app.js
+++ Javascript Tutorial and Projects/basicsES6_5/app.js
@@ -46,3 +46,60 @@ for (const fruit of fruits) {
   }
   console.log(fruit);
 }
+
+////// Spread Operator ...
+// Allows an iterable to spread/expand individually inside reciever
+// Split into single items and copy them
+
+const udemy = "udemy";
+const letters = [...udemy];
+console.log(letters); // (5) ['u', 'd', 'e', 'm', 'y']
+
+const boys = ["john", "peter", "bob"];
+const girls = ["susan", "anna"];
+const bestFriend = "arnold";
+
+const friends = [...boys, bestFriend, ...girls];
+console.log(friends); // (6) ['john', 'peter', 'bob', 'arnold', 'susan', 'anna']
+
+// reference
+// const newFriends = friends;
+const newFriends = [...friends]; // copy of the items (not a reference to original array)
+newFriends[0] = "waldemar";
+console.log(newFriends); // ['waldemar', 'peter', 'bob', 'arnold', 'susan', 'anna']
+console.log(friends); // ['john', 'peter', 'bob', 'arnold', 'susan', 'anna']
+
+/////// Spread Operator ...
+// objects
+// ES2018 - ES8
+
+const person2 = { name: "john", job: "developer" };
+const newPerson = { ...person2, city: "berlin", name: "peter" };
+console.log(newPerson); // {name: 'peter', job: 'developer', city: 'berlin'}
+// name peoperty was overriden from john to peter
+
+///// Spread Operator - DOM Elements
+const headings = document.querySelectorAll("h1");
+const result = document.getElementById("result");
+
+// console.log(headings.map((item) => console.log(item))); // error headings is not an array
+
+const text = [...headings]
+  .map((item) => `<span>${item.textContent}</span>`)
+  .join(` `);
+
+result.innerHTML = text;
+
+///////// Spread Operator - Functions Arguments
+const numbers = [23, 66, 85, 23, 74, 34, 71];
+
+console.log(Math.max(numbers)); // NaN - we cannot pass an array directly to a max() function
+console.log(Math.max(...numbers)); // 85
+
+const john2 = ["John", "Paul 2"];
+
+const sayHello = (name, lastName) => {
+  console.log(`Hello ${name}, ${lastName}`);
+};
+
+sayHello(...john2); // Hello John, Paul 2

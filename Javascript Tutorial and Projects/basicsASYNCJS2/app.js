1067cfca99ba43febfe8076ee8e63312209ffd63
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-07-10 11:05:17 +0200

    js study ASYNC JS 96
    1. promises

--- Javascript Tutorial and Projects/basicsASYNCJS2/app.js
+++ Javascript Tutorial and Projects/basicsASYNCJS2/app.js
@@ -1,16 +1,28 @@
+// PROMISES - Pending, Resolved, Rejected
+// then catch - pass another callback
+// Promises are a way to handle asynchronous operations.
 const heading1 = document.querySelector(".one");
 const heading2 = document.querySelector(".two");
 const heading3 = document.querySelector(".three");
 const btn = document.querySelector(".btn");
 
-btn.addEventListener("click", () => {
-  setTimeout(() => {
-    heading1.style.color = "red";
-    setTimeout(() => {
-      heading2.style.color = "green";
-      setTimeout(() => {
-        heading3.style.color = "blue";
-      }, 1000);
-    }, 2000);
-  }, 1000);
+btn.addEventListener("click", () => {});
+
+const promise = new Promise((resolve, reject) => {
+  let value = true;
+  if (value) {
+    resolve(`value is true`); // this might be data from a server
+  } else {
+    reject(`there was an error, value is false`);
+  }
 });
+
+console.log(promise); // Promise {<fulfilled>: 'value is true'}
+
+promise
+  .then((data) => {
+    console.log(data); // value is true
+  })
+  .catch((err) => {
+    console.log(err); // there was an error, value is false
+  });

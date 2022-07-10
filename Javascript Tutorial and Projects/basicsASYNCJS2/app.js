68424cd9728db7dd2ae6bbea1dfd9280073fca95
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-07-10 15:24:15 +0200

    js study ASYNC JS 99
    1. async, await

--- Javascript Tutorial and Projects/basicsASYNCJS2/app.js
+++ Javascript Tutorial and Projects/basicsASYNCJS2/app.js
@@ -1,16 +1,27 @@
-// PROMISES - Pending, Resolved, Rejected
-// then catch - pass another callback
-// Promises are a way to handle asynchronous operations.
+// callbacks, promises, async / await
+// in order to use await you need to put async in front of a function
+// must have async
+// await waits till the promise is resolved
+// error handling  - try / catch
+// async function returns a promise
+
+// async function someFunction() {
+//     await
+// }
+
+// const otherFunction = async() => {
+//     await
+// }
+
 const heading1 = document.querySelector(".one");
 const heading2 = document.querySelector(".two");
 const heading3 = document.querySelector(".three");
 const btn = document.querySelector(".btn");
 
-btn.addEventListener("click", () => {
-  addColor(1000, heading1, "red")
-    .then(() => addColor(2000, heading2, "green"))
-    .then(() => addColor(1000, heading3, "blue"))
-    .catch((err) => console.log(err));
+btn.addEventListener("click", async () => {
+  //   displayColor().then((data) => console.log(data));
+  const result = await displayColor();
+  console.log(result);
 });
 
 function addColor(time, element, color) {
@@ -25,4 +36,24 @@ function addColor(time, element, color) {
       reject(new Error(`Element ${element} does not exist`));
     }
   });
+}
+
+async function displayColor() {
+  try {
+    await addColor(1000, heading1, "red");
+    await addColor(2000, heading2, "green");
+    await addColor(1000, heading3, "blue");
+  } catch (err) {
+    console.log(err);
+  }
+
+  // await will wait till the promise is resolved
+  // so the code below works the same as the code above
+
+  //   addColor(1000, heading1, "red")
+  //     .then(() => addColor(2000, heading2, "green"))
+  //     .then(() => addColor(1000, heading3, "blue"))
+  //     .catch((err) => console.log(err));
+
+  return "colors changed";
 }

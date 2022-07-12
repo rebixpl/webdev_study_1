df052e7efaef77b21c33b5978131cdf7fe567d55
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-07-12 20:42:28 +0200

    js study AJAX 101
    1. Add Button
    2.  JSON

--- Javascript Tutorial and Projects/basicsAJAX1/app.js
+++ Javascript Tutorial and Projects/basicsAJAX1/app.js
@@ -1,20 +1,39 @@
+// JSON - Javascript Object Notation
+
+const btn = document.querySelector(".btn");
 const xhr = new XMLHttpRequest();
+const url = "./api/people.json";
 
-xhr.open("GET", "./api/sample.txt");
+btn.addEventListener("click", () => {
+  fetchData();
+});
 
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    console.log(xhr.responseText);
-    const text = document.createElement("p");
-    text.textContent = xhr.responseText;
-    document.body.appendChild(text);
-  } else {
-    console.log({
-      status: xhr.status,
-      text: xhr.statusText,
-      readyState: xhr.readyState,
-    });
-  }
-};
+function fetchData() {
+  xhr.open("GET", url);
 
-xhr.send();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      console.log(xhr.responseText);
+      //   const text = document.createElement("p");
+      //   text.textContent = xhr.responseText;
+      //   document.body.appendChild(text);
+      const data = JSON.parse(xhr.responseText);
+      const displayData = data
+        .map((item) => {
+          return `<p>${item.id}. ${item.name}</p>`;
+        })
+        .join("");
+      const element = document.createElement("div");
+      element.innerHTML = displayData;
+      document.body.appendChild(element);
+    } else {
+      console.log({
+        status: xhr.status,
+        text: xhr.statusText,
+        readyState: xhr.readyState,
+      });
+    }
+  };
+
+  xhr.send();
+}

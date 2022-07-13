51c790e641d708d06923acb71bf089588eeaf280
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-07-13 12:54:16 +0200

    js study AJAX 108
    1. Random User - Fetch User

--- Javascript Tutorial and Projects/javascript-course-basic-ajax-projects-master/2-random-user/setup/app.js
+++ Javascript Tutorial and Projects/javascript-course-basic-ajax-projects-master/2-random-user/setup/app.js
@@ -1,10 +1,18 @@
 import get from "./utils/getElement.js";
-
-const url = "https://randomuser.me/api/";
+import getUser from "./utils/fetchUser.js";
 
 const img = get(".user-img");
 const title = get(".user-title");
 const value = get(".user-value");
 const btn = get(".btn");
-
 const btns = get(".icon", true);
+
+const showUser = async () => {
+  // get user from API
+  const person = await getUser();
+  // display user
+  console.log(person);
+};
+
+window.addEventListener("DOMContentLoaded", showUser);
+btn.addEventListener("click", showUser);

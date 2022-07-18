755fa42d56535175fb5b0e0bf333c1f4aa973df6
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-07-18 23:02:13 +0200

    js study ES6 Stripe Submenus 122
    1. submenu links
    2. submenu - hide and column layout

--- Javascript Tutorial and Projects/javascript-basic-projects-master/26-stripe-submenus/setup/app.js
+++ Javascript Tutorial and Projects/javascript-basic-projects-master/26-stripe-submenus/setup/app.js
@@ -43,9 +43,39 @@ linkBtns.forEach((btn) => {
     const center = (tempButton.left + tempButton.right) / 2;
     const bottom = tempButton.bottom - 3;
 
-    submenu.style.left = `${center}px`;
-    submenu.style.top = `${bottom}px`;
+    const tempPage = sublinks.find(({ page }) => page === text);
+    if (tempPage) {
+      const { page, links } = tempPage;
+      submenu.style.left = `${center}px`;
+      submenu.style.top = `${bottom}px`;
+      submenu.classList.add("show");
 
-    submenu.classList.add("show");
+      let columns = "col-2";
+      if (links.length === 3) columns = "col-3";
+      if (links.length > 3) columns = "col-4";
+
+      submenu.innerHTML = `
+      <section>
+      <h4>${page}</h4>
+      <div class="submenu-center ${columns}">
+        ${links
+          .map((link) => {
+            return `<a href="${link.url}"><i class="${link.icon}"></i>${link.label}</a>`;
+          })
+          .join("")}
+      </div>
+      </section>
+      `;
+    }
   });
+});
+
+hero.addEventListener("mouseover", function () {
+  submenu.classList.remove("show");
+});
+
+nav.addEventListener("mouseover", function (e) {
+  if (!e.target.classList.contains("link-btn")) {
+    submenu.classList.remove("show");
+  }
 });

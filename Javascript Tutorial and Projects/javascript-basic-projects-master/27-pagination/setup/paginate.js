9a3379059ce605b5bc512acdce230d63d48cd42c
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-07-20 15:11:17 +0200

    js study ES6 Pagination 125
    1. Paginate - Create Pages

--- Javascript Tutorial and Projects/javascript-basic-projects-master/27-pagination/setup/paginate.js
+++ Javascript Tutorial and Projects/javascript-basic-projects-master/27-pagination/setup/paginate.js
@@ -1,3 +1,11 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const itemsPerPage = 8;
+  const numberOfPages = Math.ceil(followers.length / itemsPerPage);
+  const newFollowers = Array.from({ length: numberOfPages }, (_, index) => {
+    const startIndex = index * itemsPerPage;
+    return followers.slice(startIndex, startIndex + itemsPerPage);
+  });
+  return newFollowers;
+};
 
-export default paginate
+export default paginate;

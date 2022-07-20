fd9863754d9151e621888c5f69b037d382aee02e
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-07-20 21:14:27 +0200

    js study ES6 Pagination 127
    1. Button Functionality

--- Javascript Tutorial and Projects/javascript-basic-projects-master/27-pagination/setup/app.js
+++ Javascript Tutorial and Projects/javascript-basic-projects-master/27-pagination/setup/app.js
@@ -9,8 +9,8 @@ const btnContainer = document.querySelector(".btn-container");
 let index = 0;
 let pages = [];
 
-const setupUI = (followers) => {
-  displayFollowers(paginate(followers)[index]);
+const setupUI = () => {
+  displayFollowers(pages[index]);
   displayButtons(btnContainer, index, pages);
 };
 
@@ -18,7 +18,29 @@ const init = async () => {
   const followers = await fetchFollowers();
   title.textContent = "pagination";
   pages = paginate(followers);
-  setupUI(followers);
+  setupUI();
 };
+
+btnContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("btn-container")) return;
+  if (e.target.classList.contains("page-btn")) {
+    index = parseInt(e.target.dataset.index);
+  }
+
+  if (e.target.classList.contains("next-btn")) {
+    index++;
+    if (index > pages.length - 1) {
+      index = 0;
+    }
+  }
+  if (e.target.classList.contains("prev-btn")) {
+    index--;
+    if (index < 0) {
+      index = pages.length - 1;
+    }
+  }
+
+  setupUI();
+});
 
 window.addEventListener("load", init);

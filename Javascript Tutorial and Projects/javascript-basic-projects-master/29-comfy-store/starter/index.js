fa788fb65031a9123d9c1dc5d2b47db5ef859124
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-07-21 18:25:00 +0200

    js study Comfy Store 129
    1. Sidebar - HTML
    2. Sidebar Toggle

--- Javascript Tutorial and Projects/javascript-basic-projects-master/29-comfy-store/starter/index.js
+++ Javascript Tutorial and Projects/javascript-basic-projects-master/29-comfy-store/starter/index.js
@@ -1,9 +1,10 @@
-// global imports
-import './src/toggleSidebar.js';
-import './src/cart/toggleCart.js';
-import './src/cart/setupCart.js';
-// specific imports
-import fetchProducts from './src/fetchProducts.js';
-import { setupStore, store } from './src/store.js';
-import display from './src/displayProducts.js';
-import { getElement } from './src/utils.js';
+// global imports - used in every page
+import "./src/toggleSidebar.js";
+import "./src/cart/toggleCart.js";
+import "./src/cart/setupCart.js";
+
+// specific imports - used in specific page
+import fetchProducts from "./src/fetchProducts.js";
+import { setupStore, store } from "./src/store.js";
+import display from "./src/displayProducts.js";
+import { getElement } from "./src/utils.js";

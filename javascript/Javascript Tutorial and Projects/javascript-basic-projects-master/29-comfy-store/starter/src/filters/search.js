d76f8c7bb27ebfea2cd01f99324fe46ef1d5033a
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-07-24 22:58:30 +0200

    js study Comfy Store 139
    1. search filter

--- javascript/Javascript Tutorial and Projects/javascript-basic-projects-master/29-comfy-store/starter/src/filters/search.js
+++ javascript/Javascript Tutorial and Projects/javascript-basic-projects-master/29-comfy-store/starter/src/filters/search.js
@@ -1,5 +1,28 @@
-import { getElement } from '../utils.js';
-import display from '../displayProducts.js';
-const setupSearch = () => {};
+import { getElement } from "../utils.js";
+import display from "../displayProducts.js";
+
+const setupSearch = (store) => {
+  const form = getElement(".input-form");
+  const nameInput = getElement(".search-input");
+  form.addEventListener("keyup", function () {
+    const value = nameInput.value;
+    const productsContainer = getElement(".products-container");
+
+    if (value) {
+      const newStore = store.filter((product) => {
+        if (product.name.toLowerCase().startsWith(value.toLowerCase())) {
+          return product;
+        }
+      });
+      display(newStore, productsContainer);
+
+      if (newStore.length < 1) {
+        productsContainer.innerHTML = `<h3 class="filter-error"> sorry, no products matched your search</h3>`;
+      }
+    } else {
+      display(store, productsContainer);
+    }
+  });
+};
 
 export default setupSearch;

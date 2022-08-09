b8f4b9d2b4f235c3fe71c092a727c7a3fe60fb73
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-08-09 09:24:44 +0200

    js study What is JSX - 180
    1. Converting HTML JSX
    2. Inline Styling with JSX in React JS
    3. JavaScript Variable in JSX

--- react/Full Stack Web Development Bootcamp with MERN Stack Projects/first-app/src/index.js
+++ react/Full Stack Web Development Bootcamp with MERN Stack Projects/first-app/src/index.js
@@ -1,8 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// function getButtonText() {
+//   return "Click me!";
+// }
+
 const App = () => {
-  return <div>Hi There!</div>;
+  //   const buttonText = "Click me!";
+  //   const buttonText = [5, 52, " hello", " world"];
+  const buttonText = { text: "Submit" };
+
+  return (
+    <div>
+      <label htmlFor="name" className="label">
+        enter e-mail
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "red", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));

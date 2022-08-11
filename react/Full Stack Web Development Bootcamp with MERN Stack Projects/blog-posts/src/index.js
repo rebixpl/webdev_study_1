11b274e77556285745fdeef6dee627fc0ea7dc77
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-08-11 12:27:02 +0200

    js study React with Props - 183
    1. Duplicating The Single Component in React JS
    2. Component Nesting in React JS
    3. Props System in React JS
    4. Passing Props to Child in React JS

--- react/Full Stack Web Development Bootcamp with MERN Stack Projects/blog-posts/src/index.js
+++ react/Full Stack Web Development Bootcamp with MERN Stack Projects/blog-posts/src/index.js
@@ -1,24 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SingleComment from "./SingleComment";
 import profile1 from "./images/profilepic1.png";
+import profile2 from "./images/profilepic2.jpg";
+import profile3 from "./images/profilepic3.jpg";
 
 const App = () => {
   return (
     <div className="ui comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img src={profile1} alt="profile picture" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Waldemar
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 5:00PM</span>
-          </div>
-          <div className="text">It's amazing!</div>
-        </div>
-      </div>
+      <SingleComment
+        name="Baryla"
+        date="Today at 5:00PM"
+        text="This is nice!"
+        picture={profile1}
+      />
+      <SingleComment
+        name="Waldemar"
+        date="Today at 2:00PM"
+        text="Hello!"
+        picture={profile2}
+      />
+      <SingleComment
+        name="Uszko"
+        date="Today at 7:00PM"
+        text="widzisz mnie?"
+        picture={profile3}
+      />
     </div>
   );
 };

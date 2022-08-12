2298eacd2d55048ca118b3bced847e02f81f8e22
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-08-12 19:57:24 +0200

    js study Class - State in React - 191
    1. React JS Get to Know Lifecycle Method
    2. Refactoring App with Lifecycle Method
    3. React JS Passing State As Props

--- react/Full Stack Web Development Bootcamp with MERN Stack Projects/hemisphere/src/HemisphereDisplay.js
+++ react/Full Stack Web Development Bootcamp with MERN Stack Projects/hemisphere/src/HemisphereDisplay.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const HemisphereDisplay = () => {
+const HemisphereDisplay = (props) => {
   return <div>Hey, you are in Northern hemisphere!</div>;
 };
 
--- react/Full Stack Web Development Bootcamp with MERN Stack Projects/hemisphere/src/index.js
+++ react/Full Stack Web Development Bootcamp with MERN Stack Projects/hemisphere/src/index.js
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import HemisphereDisplay from "./HemisphereDisplay";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    // the state object is the only one that is mutable
-    this.state = {
-      latitude: null,
-      errorMessage: "",
-    };
+  //   // the state object is the only one that is mutable
+  //   this.state = {
+  //     latitude: null,
+  //     errorMessage: "",
+  //   };
+  // }
 
+  state = {
+    latitude: null,
+    errorMessage: "",
+  };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({
@@ -31,7 +39,11 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.latitude) {
-      return <div> {this.state.latitude}</div>;
+      return (
+        <div>
+          <HemisphereDisplay latitude={this.state.latitude} />
+        </div>
+      );
     } else {
       return <div>Loading...</div>;
     }

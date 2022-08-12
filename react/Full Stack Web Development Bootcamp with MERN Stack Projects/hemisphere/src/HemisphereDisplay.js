bce5e089f7bef27eeb82e746882f08ac53a94b21
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-08-12 20:23:52 +0200

    js study Class - React JS Lifecycle Methods - 192
    1. Ternary Expressions in JSX
    2. React JS Showing Picture
    3. React JS Styling The App

--- react/Full Stack Web Development Bootcamp with MERN Stack Projects/hemisphere/src/HemisphereDisplay.js
+++ react/Full Stack Web Development Bootcamp with MERN Stack Projects/hemisphere/src/HemisphereDisplay.js
@@ -1,7 +1,35 @@
 import React from "react";
+import "./Hemisphere.css";
+import northernPic from "./image/NorthernHemisphere.jpeg";
+import southernPic from "./image/SouthernHemisphere.jpeg";
 
-const HemisphereDisplay = (props) => {
-  return <div>Hey, you are in Northern hemisphere!</div>;
+const hemisphereConfig = {
+  Northern: {
+    text: "Hey, you are in Northern Hemisphere!",
+    picture: northernPic,
+  },
+  Southern: {
+    text: "Hey, you are in Southern Hemisphere!",
+    picture: southernPic,
+  },
+};
+
+const HemisphereDisplay = ({ latitude }) => {
+  const hemisphere = latitude > 0 ? "Northern" : "Southern";
+  const { text, picture } = hemisphereConfig[hemisphere];
+
+  return (
+    <div className={hemisphere}>
+      <div className="ui raised text container segment">
+        <div className="image">
+          <img src={picture} alt={hemisphere} />
+        </div>
+        <div className="ui teal bottom attached label">
+          <h1>{text}</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HemisphereDisplay;

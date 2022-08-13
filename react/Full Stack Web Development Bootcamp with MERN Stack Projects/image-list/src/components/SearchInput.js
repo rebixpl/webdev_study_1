88ecfcb28b34b6c0dcf1e8bce20dd13a928bb0dd
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-08-13 12:09:14 +0200

    js study - User Inputs, Forms and Events in React JS - 195
    1. Styling The App
    2. Creating Event Handlers
    3. Controlled vs Uncontrolled Elements in React JS

--- react/Full Stack Web Development Bootcamp with MERN Stack Projects/image-list/src/components/SearchInput.js
+++ react/Full Stack Web Development Bootcamp with MERN Stack Projects/image-list/src/components/SearchInput.js
@@ -1,11 +1,25 @@
 import React from "react";
 
 class SearchInput extends React.Component {
+  state = { entry: "" };
+
   render() {
     return (
-      <div>
-        <form action="">
-          <input type="text" />
+      <div className="ui segment">
+        <form className="ui form" action="">
+          <div className="field">
+            <div className="ui massive icon input">
+              <input
+                type="text"
+                placeholder="Search..."
+                onChange={(e) => {
+                  this.setState({ entry: e.target.value });
+                }}
+                value={this.state.entry}
+              />
+              <i className="search icon" />
+            </div>
+          </div>
         </form>
       </div>
     );

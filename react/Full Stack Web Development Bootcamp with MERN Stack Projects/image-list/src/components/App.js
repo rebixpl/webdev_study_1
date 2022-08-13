a7b03c7a2f2472ffdb59b208335f04f51c45a336
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-08-14 00:24:26 +0200

    react study - API Request with React - 197
    1. Fetching Data in React JS
    2. Axios vs Fetch in React JS
    3. Async Await Function in React JS
    4. Setting The State in React JS

--- react/Full Stack Web Development Bootcamp with MERN Stack Projects/image-list/src/components/App.js
+++ react/Full Stack Web Development Bootcamp with MERN Stack Projects/image-list/src/components/App.js
@@ -1,15 +1,24 @@
 import React from "react";
+import axios from "axios";
 import SearchInput from "./SearchInput";
 
+const API_KEY = `29252871-5ea06025819c767b9728e00ec`;
+
 class App extends React.Component {
-  onSearchSubmit = (entry) => {
-    console.log(entry);
+  state = { images: [] };
+
+  onSearchSubmit = async (entry) => {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=${API_KEY}&q=${entry}&image_type=photo`
+    );
+    this.setState({ images: response.data.hits });
   };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "30px" }}>
         <SearchInput onSearchSubmit={this.onSearchSubmit} />
+        We have {this.state.images.length} images.
       </div>
     );
   }

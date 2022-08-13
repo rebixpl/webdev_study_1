933faf9996a2969842ce9e483ec2f655c45fa517
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-08-13 21:25:57 +0200

    js study - User Inputs, Forms and Events in React JS - 196
    1. Why Controlled Elements in React JS
    2. Understanding ‘this’ Key Word In JS
    3. Communicating Child to Parent in React JS
    4. Invoking Callbacks In Children

--- react/Full Stack Web Development Bootcamp with MERN Stack Projects/image-list/src/components/SearchInput.js
+++ react/Full Stack Web Development Bootcamp with MERN Stack Projects/image-list/src/components/SearchInput.js
@@ -3,10 +3,15 @@ import React from "react";
 class SearchInput extends React.Component {
   state = { entry: "" };
 
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSearchSubmit(this.state.entry);
+  };
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form" action="">
+        <form onSubmit={this.onFormSubmit} className="ui form" action="">
           <div className="field">
             <div className="ui massive icon input">
               <input

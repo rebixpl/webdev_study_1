bb2a8c3a511f815ca1a1cb58fb24be87624ed582
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-08-17 18:37:14 +0200

    react study - Redux in ReactJS - 205
    code on codepen: https://codepen.io/rebixpl/pen/vYRvBJB
    
    1. Intro to Redux
    2. Redux Stores
    3. Redux Actions
    4. Reducers in React JS
    5. Store Subscription in ReactJS
    6. Setting Up Redux

--- react/Full Stack Web Development Bootcamp with MERN Stack Projects/react-router/src/index.js
+++ react/Full Stack Web Development Bootcamp with MERN Stack Projects/react-router/src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { legacy_createStore as createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers/rootReducer";
+
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );

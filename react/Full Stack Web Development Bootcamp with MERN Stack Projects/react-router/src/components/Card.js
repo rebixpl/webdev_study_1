0c8f30253b0d9f572e2a30c240a948ef866a290a
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-08-19 09:12:39 +0200

    react study - Redux in ReactJS - 208
    1. Card Page in React Projects

--- react/Full Stack Web Development Bootcamp with MERN Stack Projects/react-router/src/components/Card.js
+++ react/Full Stack Web Development Bootcamp with MERN Stack Projects/react-router/src/components/Card.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { connect } from 'react-redux';
 
 function withParams(Component) {
   return (props) => <Component {...props} params={useParams()} />;
 }
 
 class Card extends React.Component {
-  state = { user: "" };
+  // state = { user: "" };
 
-  componentDidMount() {
-    let user = this.props.params.user;
-    this.setState({ user }); // like this.setState({ user: user }); but more efficient and less code
-  }
+  // componentDidMount() {
+  //   let user = this.props.params.user;
+  //   this.setState({ user }); // like this.setState({ user: user }); but more efficient and less code
+  // }
 
   render() {
-    const { user } = this.state;
+    const { title,body } = this.props.card;
     return (
       <div
         className="ui raised very padded text container segment"
@@ -24,11 +25,21 @@ class Card extends React.Component {
           <h3 className="ui header textcenter">Card</h3>
         </div>
         <h3 className="ui header">
-          Welcome Back <span style={{ color: "green" }}>{user}</span>!
+          Welcome Back <span style={{ color: "green" }}>{title}</span>!
         </h3>
+        <p>{body}</p>
       </div>
     );
   }
 }
 
-export default withParams(Card);
+const mapStateToProps = (state, ownProps) => {
+  let title = ownProps.params.user;
+  console.log(title);
+  
+  return { 
+    card: state.cards.find(card => card.title === title)
+  }
+}
+
+export default withParams(connect(mapStateToProps)(Card));

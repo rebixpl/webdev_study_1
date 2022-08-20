646e9abade3047a64a3da33a7d5331446236f5fa
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-08-20 19:21:57 +0200

    react study - React Context - 213
    1. Lets Reach Out The Context State in react js
    2. Consuming Data in TodoList Component with React JS

--- react/Full Stack Web Development Bootcamp with MERN Stack Projects/contextapi/src/components/Navbar.js
+++ react/Full Stack Web Development Bootcamp with MERN Stack Projects/contextapi/src/components/Navbar.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends React.Component {
+  static contextType = ThemeContext;
   render() {
+    // console.log(this.context);
+    const { isDarkTheme, darkTheme, lightTheme } = this.context;
+    const theme = isDarkTheme ? darkTheme : lightTheme;
     return (
-      <nav>
-        <h2 className="ui centered header">Adam Nipplesucker's TODO App</h2>
+      <nav
+        style={{
+          background: theme.background,
+          color: theme.text,
+          height: "120px",
+        }}
+      >
+        <h2 style={{ textAlign: "center" }}>Adam Nipplesucker's TODO App</h2>
         <div className="ui three buttons">
           <button className="ui button">Overview</button>
           <button className="ui button">Contact</button>

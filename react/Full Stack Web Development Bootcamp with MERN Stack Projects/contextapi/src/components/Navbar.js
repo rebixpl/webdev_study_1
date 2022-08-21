0da8a7900cd9d09c9604ed35d294501eb3ac72c1
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-08-21 09:39:46 +0200

    react study - React Context - 215
    1. Context Consumer in React JS Hooks and Context

--- react/Full Stack Web Development Bootcamp with MERN Stack Projects/contextapi/src/components/Navbar.js
+++ react/Full Stack Web Development Bootcamp with MERN Stack Projects/contextapi/src/components/Navbar.js
@@ -2,26 +2,38 @@ import React from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends React.Component {
-  static contextType = ThemeContext;
+  // contextType works only in class Components
+  // static contextType = ThemeContext;
   render() {
     // console.log(this.context);
-    const { isDarkTheme, darkTheme, lightTheme } = this.context;
-    const theme = isDarkTheme ? darkTheme : lightTheme;
+    // const { isDarkTheme, darkTheme, lightTheme } = this.context;
+    // const theme = isDarkTheme ? darkTheme : lightTheme;
     return (
-      <nav
-        style={{
-          background: theme.background,
-          color: theme.text,
-          height: "120px",
+      // Context Consumer works in both functional and class Components
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isDarkTheme, darkTheme, lightTheme } = context;
+          const theme = isDarkTheme ? darkTheme : lightTheme;
+          return (
+            <nav
+              style={{
+                background: theme.background,
+                color: theme.text,
+                height: "120px",
+              }}
+            >
+              <h2 style={{ textAlign: "center" }}>
+                Adam Nipplesucker's TODO App
+              </h2>
+              <div className="ui three buttons">
+                <button className="ui button">Overview</button>
+                <button className="ui button">Contact</button>
+                <button className="ui button">Support</button>
+              </div>
+            </nav>
+          );
         }}
-      >
-        <h2 style={{ textAlign: "center" }}>Adam Nipplesucker's TODO App</h2>
-        <div className="ui three buttons">
-          <button className="ui button">Overview</button>
-          <button className="ui button">Contact</button>
-          <button className="ui button">Support</button>
-        </div>
-      </nav>
+      </ThemeContext.Consumer>
     );
   }
 }

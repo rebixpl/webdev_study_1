95276c54270ff7a54045a98d5d6a608042a5caf4
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-08-21 13:04:53 +0200

    react study - React Hooks - 217
    1. Use Multiple Context Inside One Component in React js hooks
    2. Hooks Intro

--- react/Full Stack Web Development Bootcamp with MERN Stack Projects/contextapi/src/components/Navbar.js
+++ react/Full Stack Web Development Bootcamp with MERN Stack Projects/contextapi/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { AuthContext } from "../contexts/AuthContext";
 import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends React.Component {
@@ -9,32 +10,44 @@ class Navbar extends React.Component {
     // const { isDarkTheme, darkTheme, lightTheme } = this.context;
     // const theme = isDarkTheme ? darkTheme : lightTheme;
     return (
-      // Context Consumer works in both functional and class Components
-      <ThemeContext.Consumer>
-        {(context) => {
-          console.log(context);
-          const { isDarkTheme, darkTheme, lightTheme } = context;
-          const theme = isDarkTheme ? darkTheme : lightTheme;
+      <AuthContext.Consumer>
+        {(authContext) => {
           return (
-            <nav
-              style={{
-                background: theme.background,
-                color: theme.text,
-                height: "120px",
+            // Context Consumer works in both functional and class Components
+            <ThemeContext.Consumer>
+              {(themeContext) => {
+                const { isDarkTheme, darkTheme, lightTheme } = themeContext;
+                const { isLoggedIn, changeAuthStatus } = authContext;
+                const theme = isDarkTheme ? darkTheme : lightTheme;
+                return (
+                  <nav
+                    style={{
+                      background: theme.background,
+                      color: theme.text,
+                      height: "120px",
+                    }}
+                  >
+                    <h2 style={{ textAlign: "center" }}>
+                      Adam Nipplesucker's TODO App
+                    </h2>
+                    <p
+                      onClick={changeAuthStatus}
+                      style={{ textAlign: "center" }}
+                    >
+                      {isLoggedIn ? "Logged In" : "Logged Out"}
+                    </p>
+                    <div className="ui three buttons">
+                      <button className="ui button">Overview</button>
+                      <button className="ui button">Contact</button>
+                      <button className="ui button">Support</button>
+                    </div>
+                  </nav>
+                );
               }}
-            >
-              <h2 style={{ textAlign: "center" }}>
-                Adam Nipplesucker's TODO App
-              </h2>
-              <div className="ui three buttons">
-                <button className="ui button">Overview</button>
-                <button className="ui button">Contact</button>
-                <button className="ui button">Support</button>
-              </div>
-            </nav>
+            </ThemeContext.Consumer>
           );
         }}
-      </ThemeContext.Consumer>
+      </AuthContext.Consumer>
     );
   }
 }

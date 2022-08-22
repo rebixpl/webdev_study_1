ff632e721adfa61acc95b12d8eb97aa51db562a7
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-08-22 15:32:10 +0200

    react study - React Hooks - 221
    1. UseContext Hook
    2. Create Multiple Context with UseContext

--- react/Full Stack Web Development Bootcamp with MERN Stack Projects/contextapi/src/components/Navbar.js
+++ react/Full Stack Web Development Bootcamp with MERN Stack Projects/contextapi/src/components/Navbar.js
@@ -1,55 +1,84 @@
-import React from "react";
+import React, { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-class Navbar extends React.Component {
-  // contextType works only in class Components
-  // static contextType = ThemeContext;
-  render() {
-    // console.log(this.context);
-    // const { isDarkTheme, darkTheme, lightTheme } = this.context;
-    // const theme = isDarkTheme ? darkTheme : lightTheme;
-    return (
-      <AuthContext.Consumer>
-        {(authContext) => {
-          return (
-            // Context Consumer works in both functional and class Components
-            <ThemeContext.Consumer>
-              {(themeContext) => {
-                const { isDarkTheme, darkTheme, lightTheme } = themeContext;
-                const { isLoggedIn, changeAuthStatus } = authContext;
-                const theme = isDarkTheme ? darkTheme : lightTheme;
-                return (
-                  <nav
-                    style={{
-                      background: theme.background,
-                      color: theme.text,
-                      height: "120px",
-                    }}
-                  >
-                    <h2 style={{ textAlign: "center" }}>
-                      Adam Nipplesucker's TODO App
-                    </h2>
-                    <p
-                      onClick={changeAuthStatus}
-                      style={{ textAlign: "center" }}
-                    >
-                      {isLoggedIn ? "Logged In" : "Logged Out"}
-                    </p>
-                    <div className="ui three buttons">
-                      <button className="ui button">Overview</button>
-                      <button className="ui button">Contact</button>
-                      <button className="ui button">Support</button>
-                    </div>
-                  </nav>
-                );
-              }}
-            </ThemeContext.Consumer>
-          );
-        }}
-      </AuthContext.Consumer>
-    );
-  }
-}
+// class Navbar extends React.Component {
+//   // contextType works only in class Components
+//   // static contextType = ThemeContext;
+//   render() {
+//     // console.log(this.context);
+//     // const { isDarkTheme, darkTheme, lightTheme } = this.context;
+//     // const theme = isDarkTheme ? darkTheme : lightTheme;
+//     return (
+//       <AuthContext.Consumer>
+//         {(authContext) => {
+//           return (
+//             // Context Consumer works in both functional and class Components
+//             <ThemeContext.Consumer>
+//               {(themeContext) => {
+//                 const { isDarkTheme, darkTheme, lightTheme } = themeContext;
+//                 const { isLoggedIn, changeAuthStatus } = authContext;
+//                 const theme = isDarkTheme ? darkTheme : lightTheme;
+//                 return (
+//                   <nav
+//                     style={{
+//                       background: theme.background,
+//                       color: theme.text,
+//                       height: "120px",
+//                     }}
+//                   >
+//                     <h2 style={{ textAlign: "center" }}>
+//                       Adam Nipplesucker's TODO App
+//                     </h2>
+//                     <p
+//                       onClick={changeAuthStatus}
+//                       style={{ textAlign: "center" }}
+//                     >
+//                       {isLoggedIn ? "Logged In" : "Logged Out"}
+//                     </p>
+//                     <div className="ui three buttons">
+//                       <button className="ui button">Overview</button>
+//                       <button className="ui button">Contact</button>
+//                       <button className="ui button">Support</button>
+//                     </div>
+//                   </nav>
+//                 );
+//               }}
+//             </ThemeContext.Consumer>
+//           );
+//         }}
+//       </AuthContext.Consumer>
+//     );
+//   }
+// }
+
+const Navbar = () => {
+  const themeContext = useContext(ThemeContext);
+  const authContext = useContext(AuthContext);
+
+  const { isDarkTheme, darkTheme, lightTheme } = themeContext;
+  const { isLoggedIn, changeAuthStatus } = authContext;
+  const theme = isDarkTheme ? darkTheme : lightTheme;
+
+  return (
+    <nav
+      style={{
+        background: theme.background,
+        color: theme.text,
+        height: "120px",
+      }}
+    >
+      <h2 style={{ textAlign: "center" }}>Adam Nipplesucker's TODO App</h2>
+      <p onClick={changeAuthStatus} style={{ textAlign: "center" }}>
+        {isLoggedIn ? "Logged In" : "Logged Out"}
+      </p>
+      <div className="ui three buttons">
+        <button className="ui button">Overview</button>
+        <button className="ui button">Contact</button>
+        <button className="ui button">Support</button>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- react/Full Stack Web Development Bootcamp with MERN Stack Projects/contextapi/src/components/TodoList.js
+++ react/Full Stack Web Development Bootcamp with MERN Stack Projects/contextapi/src/components/TodoList.js
@@ -1,29 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-class TodoList extends React.Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isDarkTheme, darkTheme, lightTheme, changeTheme } = this.context;
-    const theme = isDarkTheme ? darkTheme : lightTheme;
-    return (
-      <div
-        style={{
-          background: theme.background,
-          color: theme.text,
-          height: "140px",
-          textAlign: "center",
-        }}
-      >
-        <p className="item">Plan the family trip</p>
-        <p className="item">Go shopping for dinner</p>
-        <p className="item">Go for a walk</p>
-        <button className="ui button primary" onClick={changeTheme}>
-          Change The Theme
-        </button>
-      </div>
-    );
-  }
-}
+// class TodoList extends React.Component {
+//   static contextType = ThemeContext;
+//   render() {
+//     const { isDarkTheme, darkTheme, lightTheme, changeTheme } = this.context;
+//     const theme = isDarkTheme ? darkTheme : lightTheme;
+//     return (
+//       <div
+//         style={{
+//           background: theme.background,
+//           color: theme.text,
+//           height: "140px",
+//           textAlign: "center",
+//         }}
+//       >
+//         <p className="item">Plan the family trip</p>
+//         <p className="item">Go shopping for dinner</p>
+//         <p className="item">Go for a walk</p>
+//         <button className="ui button primary" onClick={changeTheme}>
+//           Change The Theme
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+const TodoList = () => {
+  const { isDarkTheme, darkTheme, lightTheme, changeTheme } =
+    useContext(ThemeContext);
+  const theme = isDarkTheme ? darkTheme : lightTheme;
+
+  return (
+    <div
+      style={{
+        background: theme.background,
+        color: theme.text,
+        height: "140px",
+        textAlign: "center",
+      }}
+    >
+      <p className="item">Plan the family trip</p>
+      <p className="item">Go shopping for dinner</p>
+      <p className="item">Go for a walk</p>
+      <button className="ui button primary" onClick={changeTheme}>
+        Change The Theme
+      </button>
+    </div>
+  );
+};
 
 export default TodoList;

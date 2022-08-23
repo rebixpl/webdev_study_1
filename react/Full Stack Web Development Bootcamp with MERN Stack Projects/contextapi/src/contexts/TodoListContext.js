af262fe0b32a1959d5383f7f4781a36d798bc088
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-08-23 10:17:45 +0200

    react study - React Hooks - 224
    1. UseReducer Hook
    2. Let's Implement UseReducer in reactjs
    3. Reducer Component Structure in React JS

--- react/Full Stack Web Development Bootcamp with MERN Stack Projects/contextapi/src/contexts/TodoListContext.js
+++ react/Full Stack Web Development Bootcamp with MERN Stack Projects/contextapi/src/contexts/TodoListContext.js
@@ -1,28 +1,35 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useReducer } from "react";
+import { todosReducer } from "../reducers/todosReducer";
 
 export const TodoListContext = createContext();
 
 const TodoListContextProvider = ({ children }) => {
-  const [todos, setTodos] = useState([
+  const [todos, dispatch] = useReducer(todosReducer, [
     { text: "Plan the family trip", id: Math.random() },
     { text: "Go shopping for dinner", id: Math.random() },
     { text: "Go for a walk", id: Math.random() },
   ]);
 
-  const addTodo = (todo) => {
-    setTodos([...todos, { text: todo, id: Math.random() }]);
-  };
+  //   const [todos, setTodos] = useState([
+  //     { text: "Plan the family trip", id: Math.random() },
+  //     { text: "Go shopping for dinner", id: Math.random() },
+  //     { text: "Go for a walk", id: Math.random() },
+  //   ]);
 
-  const removeTodo = (id) => {
-    setTodos(
-      todos.filter((todo) => {
-        return todo.id !== Number(id);
-      })
-    );
-  };
+  //   const addTodo = (todo) => {
+  //     setTodos([...todos, { text: todo, id: Math.random() }]);
+  //   };
+
+  //   const removeTodo = (id) => {
+  //     setTodos(
+  //       todos.filter((todo) => {
+  //         return todo.id !== Number(id);
+  //       })
+  //     );
+  //   };
 
   return (
-    <TodoListContext.Provider value={{ todos, addTodo, removeTodo }}>
+    <TodoListContext.Provider value={{ todos, dispatch }}>
       {children}
     </TodoListContext.Provider>
   );

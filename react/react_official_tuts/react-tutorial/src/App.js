a07daffcbaa0684a05fc4a9fcaf70c16e750b945
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-09-04 09:39:40 +0200

    reactjs study | taniarascia - 231
    https://www.taniarascia.com/getting-started-with-react/
    1. submitting form data
    2.

--- react/react_official_tuts/react-tutorial/src/App.js
+++ react/react_official_tuts/react-tutorial/src/App.js
@@ -1,26 +1,10 @@
 import React from "react"
 import Table from "./Table"
+import Form from "./From"
 
 class App extends React.Component {
   state = {
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ],
+    characters: [],
   }
 
   removeCharacter = (index) => {
@@ -33,6 +17,10 @@ class App extends React.Component {
     })
   }
 
+  handleSubmit = (character) => {
+    this.setState({ characters: [...this.state.characters, character] });
+  }
+
   render() {
     const { characters } = this.state;
 
@@ -40,6 +28,7 @@ class App extends React.Component {
       <React.StrictMode>
         <div className="container">
           <Table characterData={characters} removeCharacter={this.removeCharacter} />
+          <Form handleSubmit={this.handleSubmit} />
         </div>
       </React.StrictMode>
     )
